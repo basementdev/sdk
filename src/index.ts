b8cb0c80d50021bbcca27003ebc4cf1a3ba5cd57
Anas Yousef <anas.y0807@gmail.com>
2022-07-14 19:15:24 +0100

    feat: implement token and tokenMetadataRefresh

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { GraphQLClient } from "graphql-request";
-import { AddressQuery, AddressQueryVariables, getSdk } from "./sdk";
+import {
+  AddressQuery,
+  AddressQueryVariables,
+  getSdk,
+  TokenMetadataRefreshMutation,
+  TokenMetadataRefreshMutationVariables,
+  TokenQuery,
+  TokenQueryVariables,
+} from "./sdk";
 
 const DEFAULT_ENDPOINT = "https://api.basement.dev/graphiql";
 
@@ -35,6 +43,52 @@ export class SDK {
       includeReverseProfile,
       includeTokens,
       tokensLimit,
+    });
+  }
+
+  /**
+   * Queries information about a specific token
+   * @param {TokenQueryVariables} queryObj query variables object
+   * @param queryObj.contract contract hex-address
+   * @param queryObj.id internal ID
+   * @param queryObj.tokenId token ID
+   * @param queryObj.includeOwnerInfo Whether to include the token owner's information in the response - defaults to `false`
+   * @param queryObj.includeOwnerProfile Whether to include the owner's profile in the response. `queryObj.includeOwnerInfo` must be set to `true` for this option to take effect -  defaults to `false`
+   */
+  public async token({
+    contract,
+    id,
+    includeOwnerInfo,
+    includeOwnerProfile,
+    includeOwnerReverseProfile,
+    tokenId,
+  }: TokenQueryVariables): Promise<TokenQuery> {
+    return this.sdk.token({
+      contract,
+      id,
+      includeOwnerInfo,
+      includeOwnerProfile,
+      includeOwnerReverseProfile,
+      tokenId,
+    });
+  }
+
+  /**
+   * Refreshes metadata of a specific token
+   * @param {TokenMetadataRefreshMutationVariables} mutationObj mutation variables object
+   * @param mutationObj.contract contract hex-address
+   * @param mutationObj.id internal ID
+   * @param mutationObj.tokenId token ID
+   */
+  public async tokenMetadataRefresh({
+    contract,
+    id,
+    tokenId,
+  }: TokenMetadataRefreshMutationVariables): Promise<TokenMetadataRefreshMutation> {
+    return this.sdk.tokenMetadataRefresh({
+      contract,
+      id,
+      tokenId,
     });
   }
 }
