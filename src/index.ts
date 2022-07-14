1e97956c3da16622acdfb9f4e7c34892b37734e3
Anas Yousef <anas.y0807@gmail.com>
2022-07-14 14:35:30 +0100

    feat: implement address

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,40 @@
 import { GraphQLClient } from "graphql-request";
-import { getSdk } from "./sdk";
+import { AddressQuery, AddressQueryVariables, getSdk } from "./sdk";
 
-async function main() {
-  const client = new GraphQLClient("https://api.basement.dev/graphiql");
-  const sdk = getSdk(client);
-  const { address } = await sdk.address({
-    name: "vitalik.eth",
-    includeReverseProfile: true,
-    includeTokens: true,
-    filterSuspectedScams: true,
-    includeProfile: true,
-    tokensLimit: 10,
-  });
-  console.log(address);
+const DEFAULT_ENDPOINT = "https://api.basement.dev/graphiql";
+
+export class SDK {
+  private sdk: ReturnType<typeof getSdk>;
+  constructor() {
+    const client = new GraphQLClient(DEFAULT_ENDPOINT);
+    this.sdk = getSdk(client);
+  }
+
+  /**
+   * Queries information about an address
+   * @param {AddressQueryVariables} queryObj query variables object
+   * @param queryObj.name hex-address or ENS address
+   * @param queryObj.filterSuspectedScams *Experimental* - Whether to remove the results that are suspected to be scams - defaults to `false`
+   * @param queryObj.includeProfile Whether to include the ENS profile information in the response - defaults to `false`
+   * @param queryObj.includeReverseProfile Whether to include the reverse resolution of the ENS profile information in the response (ENS docs: https://docs.ens.domains/contract-api-reference/reverseregistrar) - defaults to `false`
+   * @param queryObj.includeTokens Whether to include the tokens that the address holds - defaults to `false`
+   * @param queryObj.tokensLimit Maximum number of tokens to return. `queryObj.includeTokens` must be set to `true` for this option to take effect - defaults to `10`
+   */
+  public async address({
+    name,
+    filterSuspectedScams,
+    includeProfile,
+    includeReverseProfile,
+    includeTokens,
+    tokensLimit,
+  }: AddressQueryVariables): Promise<AddressQuery> {
+    return this.sdk.address({
+      name,
+      filterSuspectedScams,
+      includeProfile,
+      includeReverseProfile,
+      includeTokens,
+      tokensLimit,
+    });
+  }
 }
-
-main().then();
