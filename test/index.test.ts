247cb64770f84d74a2318c47bc0aed4391e266e6
Anas Yousef <anas.y0807@gmail.com>
2022-07-14 19:21:00 +0100

    test: fix test

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,7 +2,7 @@ import { SDK } from "../src";
 
 test("test", async () => {
   const sdk = new SDK();
-  const { token } = await sdk.t({
+  const { token } = await sdk.token({
     includeOwnerInfo: true,
     contract: "0xa97d3eb991303cf3b9b759bd026bacb55256e9db",
     tokenId: "216",
