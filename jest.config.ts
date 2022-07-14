9b10e0562256d1f5546c87682adbecae34ba1654
Anas Yousef <anas.y0807@gmail.com>
2022-07-14 19:17:08 +0100

    test: structure test

--- jest.config.ts
+++ jest.config.ts
@@ -100,7 +100,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: "ts-jest",
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -135,7 +135,7 @@ export default {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ["./jest.setup.ts"],
+  // setupFilesAfterEnv: ["./test/jest.setup.ts"],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,11 @@
 import { SDK } from "../src";
 
-test("test", async () => {});
+test("test", async () => {
+  const sdk = new SDK();
+  const { token } = await sdk.t({
+    includeOwnerInfo: true,
+    contract: "0xa97d3eb991303cf3b9b759bd026bacb55256e9db",
+    tokenId: "216",
+  });
+  console.log(token.imageUrl);
+});
