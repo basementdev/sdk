66a9adb9baebb9436f844ead04af95691356ce58
Anas Yousef <anas.y0807@gmail.com>
2022-07-18 14:03:26 +0100

    test: add tests for all queries & mutations

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,13 +1,176 @@
-import { graphql } from "msw";
+import { graphql, GraphQLRequest } from "msw";
+import {
+  AddressQueryVariables,
+  TokenQueryVariables,
+  TokensQueryVariables,
+  TokenTransfersQueryVariables,
+} from "../src/sdk";
+
+type OwnerInfoOptions = {
+  includeOwnerInfo: boolean;
+  includeOwnerProfile: boolean;
+  includeOwnerReverseProfile: boolean;
+};
+
+function populateOwnerInfo({
+  includeOwnerInfo,
+  includeOwnerProfile,
+  includeOwnerReverseProfile,
+}: OwnerInfoOptions) {
+  const res: any = {};
+
+  if (includeOwnerInfo) {
+    res.ownerAddress = { address: "" };
+    if (includeOwnerProfile) {
+      res.ownerAddress.profile = {};
+    }
+    if (includeOwnerReverseProfile) {
+      res.ownerAddress.reverseProfile = {};
+    }
+  }
+
+  return res;
+}
 
 export const handlers = [
-  graphql.query("address", (req, res, ctx) => {
-    return res(
-      ctx.data({
-        user: {
-          firstname: "John",
-        },
-      })
-    );
+  graphql.query(
+    "token",
+    (req: GraphQLRequest<TokenQueryVariables>, res, ctx) => {
+      const {
+        includeOwnerInfo,
+        includeOwnerProfile,
+        includeOwnerReverseProfile,
+      } = req.variables;
+
+      const populatedOwnerInfo = populateOwnerInfo({
+        includeOwnerInfo,
+        includeOwnerProfile,
+        includeOwnerReverseProfile,
+      });
+      return res(
+        ctx.data({
+          token: {
+            ...populatedOwnerInfo,
+          },
+        })
+      );
+    }
+  ),
+
+  graphql.query(
+    "address",
+    (req: GraphQLRequest<AddressQueryVariables>, res, ctx) => {
+      const dynamicRes: any = {};
+      const {
+        includeProfile,
+        includeReverseProfile,
+        includeTokens,
+        tokensLimit,
+      } = req.variables;
+      if (includeProfile) {
+        dynamicRes.profile = {};
+      }
+      if (includeReverseProfile) {
+        dynamicRes.reverseProfile = {};
+      }
+
+      if (includeTokens) {
+        dynamicRes.tokens = new Array(tokensLimit).fill({});
+      }
+
+      return res(
+        ctx.data({
+          address: {
+            address: "",
+            ...dynamicRes,
+          },
+        })
+      );
+    }
+  ),
+
+  graphql.mutation("tokenMetadataRefresh", (_, res, ctx) => {
+    return res(ctx.data({ tokenMetadataRefresh: {} }));
   }),
+
+  graphql.query(
+    "tokens",
+    (req: GraphQLRequest<TokensQueryVariables>, res, ctx) => {
+      const {
+        includeOwnerInfo,
+        includeOwnerProfile,
+        includeOwnerReverseProfile,
+        limit,
+      } = req.variables;
+
+      const populatedOwnerInfo = populateOwnerInfo({
+        includeOwnerInfo,
+        includeOwnerProfile,
+        includeOwnerReverseProfile,
+      });
+
+      const tokensRes = new Array(limit).fill({ ...populatedOwnerInfo });
+
+      return res(
+        ctx.data({
+          tokens: {
+            tokens: tokensRes,
+          },
+        })
+      );
+    }
+  ),
+
+  graphql.query(
+    "tokenTransfers",
+    (req: GraphQLRequest<TokenTransfersQueryVariables>, res, ctx) => {
+      const dynamicRes: any = { from: { address: "" }, to: { address: "" } };
+      const {
+        includeERC721Metadata,
+        includeFromProfile,
+        includeFromReverseProfile,
+        includeFromTokensInfo,
+        includeToProfile,
+        includeToReverseProfile,
+        includeToTokensInfo,
+        limit,
+      } = req.variables;
+
+      if (includeERC721Metadata) {
+        dynamicRes.erc721Metadata = {};
+      }
+      if (includeFromProfile) {
+        dynamicRes.from = { ...dynamicRes.from, profile: {} };
+      }
+
+      if (includeFromReverseProfile) {
+        dynamicRes.from = { ...dynamicRes.from, reverseProfile: {} };
+      }
+
+      if (includeFromTokensInfo) {
+        dynamicRes.from = { ...dynamicRes.from, tokens: {} };
+      }
+
+      if (includeToProfile) {
+        dynamicRes.to = { ...dynamicRes.to, profile: {} };
+      }
+
+      if (includeToReverseProfile) {
+        dynamicRes.to = { ...dynamicRes.to, reverseProfile: {} };
+      }
+
+      if (includeToTokensInfo) {
+        dynamicRes.to = { ...dynamicRes.to, tokens: {} };
+      }
+
+      const tokenTransfersRes = new Array(limit).fill(dynamicRes);
+      return res(
+        ctx.data({
+          tokenTransfers: {
+            tokenTransfers: tokenTransfersRes,
+          },
+        })
+      );
+    }
+  ),
 ];
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,100 @@
 import { SDK } from "../src";
 
-test("test", async () => {
+describe("Basement SDK", () => {
+  const TOKEN_CONTRACT_ADDRESS = "0xa97d3eb991303cf3b9b759bd026bacb55256e9db";
+  const TOKEN_ID = "216";
   const sdk = new SDK();
-  const { token } = await sdk.token({
-    includeOwnerInfo: true,
-    contract: "0xa97d3eb991303cf3b9b759bd026bacb55256e9db",
-    tokenId: "216",
+
+  test("token query", async () => {
+    const { token } = await sdk.token({
+      includeOwnerProfile: true,
+      includeOwnerReverseProfile: true,
+      contract: TOKEN_CONTRACT_ADDRESS,
+      tokenId: TOKEN_ID,
+    });
+
+    const keys = Object.keys(token.ownerAddress);
+
+    expect(keys).toEqual(
+      expect.arrayContaining(["address", "profile", "reverseProfile"])
+    );
   });
-  console.log(token.imageUrl);
+
+  test("address query", async () => {
+    const tokensLimit = 15;
+    const { address } = await sdk.address({
+      name: "test.eth",
+      includeProfile: true,
+      includeReverseProfile: true,
+      includeTokens: true,
+      tokensLimit,
+    });
+
+    const keys = Object.keys(address);
+
+    expect(keys).toEqual(
+      expect.arrayContaining(["address", "profile", "reverseProfile"])
+    );
+
+    expect(address.tokens?.length).toBeLessThanOrEqual(tokensLimit);
+  });
+
+  test("tokenMetadataRefresh mutation", async () => {
+    const { tokenMetadataRefresh } = await sdk.tokenMetadataRefresh({
+      contract: TOKEN_CONTRACT_ADDRESS,
+      tokenId: TOKEN_ID,
+    });
+    expect(tokenMetadataRefresh).toBeDefined;
+  });
+
+  test("tokens query", async () => {
+    const tokensLimit = 5;
+    const { tokens } = await sdk.tokens({
+      filter: { contractAddress: TOKEN_CONTRACT_ADDRESS },
+      includeOwnerProfile: true,
+      includeOwnerReverseProfile: true,
+      limit: tokensLimit,
+    });
+
+    expect(tokens?.tokens?.length).toBeLessThanOrEqual(tokensLimit);
+    const token = tokens?.tokens[0];
+    const keys = Object.keys(token?.ownerAddress);
+    expect(keys).toEqual(
+      expect.arrayContaining(["address", "profile", "reverseProfile"])
+    );
+  });
+
+  test("tokenTransfers query", async () => {
+    const tokenTransfersLimit = 5;
+    const { tokenTransfers } = await sdk.tokenTransfers({
+      filter: { contractAddress: TOKEN_CONTRACT_ADDRESS },
+      limit: tokenTransfersLimit,
+      includeERC721Metadata: true,
+      includeFromProfile: true,
+      includeFromReverseProfile: true,
+      includeFromTokensInfo: true,
+      includeToProfile: true,
+      includeToReverseProfile: true,
+      includeToTokensInfo: true,
+    });
+
+    const tokenTransfer = tokenTransfers?.tokenTransfers[0];
+    const tokenTransferKeys = Object.keys(tokenTransfer);
+    const tokenTransferFromKeys = Object.keys(tokenTransfer?.from);
+    const tokenTransferToKeys = Object.keys(tokenTransfer?.to);
+    expect(tokenTransfers?.tokenTransfers?.length).toBeLessThanOrEqual(
+      tokenTransfersLimit
+    );
+
+    expect(tokenTransferKeys).toEqual(
+      expect.arrayContaining(["from", "to", "erc721Metadata"])
+    );
+
+    expect(tokenTransferFromKeys).toEqual(
+      expect.arrayContaining(["address", "profile", "reverseProfile", "tokens"])
+    );
+    expect(tokenTransferToKeys).toEqual(
+      expect.arrayContaining(["address", "profile", "reverseProfile", "tokens"])
+    );
+  });
 });
