b04869a442c7f645e112eea98db7e3ee9113db44
Anas Yousef <anas.y0807@gmail.com>
2022-07-19 15:52:56 +0100

    style: fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,31 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "airbnb-typescript",
-    "prettier",
-  ],
+  extends: ["airbnb-base", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
-  rules: {},
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".ts", ".tsx"],
+        moduleDirectory: ["src", "node_modules"],
+      },
+    },
+  },
+  rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+  },
 };
