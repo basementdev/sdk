d09a73e4df7a5d866fd74dcd504361836b044c1a
Anas Yousef <anas.y0807@gmail.com>
2022-11-10 23:35:59 +0000

    feat: implement tokens query

--- src/basement.ts
+++ src/basement.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
-import { AddressQuery, getSdk } from "./sdk";
-import { AddressQueryOptions } from "./types";
+import { AddressQuery, getSdk, TokenQuery } from "./sdk";
+import { AddressQueryOptions, TokenQueryOptions } from "./types";
 
 export const DEFAULT_ENDPOINT = "https://beta.basement.dev/v2/graphiql";
 
@@ -10,6 +10,41 @@ export class BasementSDK {
   constructor(endpoint: string = DEFAULT_ENDPOINT) {
     const client = new GraphQLClient(endpoint);
     this.sdk = getSdk(client);
+  }
+
+  /**
+   * Queries information about a specific token
+   */
+  public async token({
+    contract,
+    tokenId,
+    include,
+  }: TokenQueryOptions): Promise<TokenQuery> {
+    const includeOwnerInfo = !!include?.owner;
+    const includeOwnerProfile = include?.owner?.profile;
+    const includeOwnerReverseProfile = include?.owner?.reverseProfile;
+    const includeSales = !!include?.sales;
+    const includeMakerReverseProfile = include?.sales?.maker?.reverseProfile;
+    const includeTakerReverseProfile = include?.sales?.taker?.reverseProfile;
+    const includeMediaInfo = include?.media;
+    const includeMintInfo = !!include?.mint;
+    const includeMintTransactionLogs = include?.mint?.transactionLogs;
+    const includeTokenUri = include?.tokenUri;
+
+    return this.sdk.token({
+      contract,
+      tokenId,
+      includeOwnerInfo,
+      includeOwnerProfile,
+      includeOwnerReverseProfile,
+      includeSales,
+      includeTakerReverseProfile,
+      includeMakerReverseProfile,
+      includeMediaInfo,
+      includeMintInfo,
+      includeMintTransactionLogs,
+      includeTokenUri,
+    });
   }
 
   /**
@@ -23,12 +58,28 @@ export class BasementSDK {
     const tokensLimit = include?.tokens?.limit;
     const includeProfile = include?.profile;
     const includeReverseProfile = include?.reverseProfile;
+    const includeSales = !!include?.tokens?.sales;
+    const includeMediaInfo = include?.tokens?.media;
+    const includeMakerReverseProfile =
+      include?.tokens?.sales?.maker?.reverseProfile;
+    const includeTakerReverseProfile =
+      include?.tokens?.sales?.taker?.reverseProfile;
+    const includeMintInfo = !!include?.tokens?.mint;
+    const includeMintTransactionLogs = include?.tokens?.mint?.transactionLogs;
+    const includeTokenUri = include?.tokens?.tokenUri;
     const data = await this.sdk.address({
       address,
       includeProfile,
       includeReverseProfile,
       includeTokens,
       tokensLimit,
+      includeSales,
+      includeMediaInfo,
+      includeMintInfo,
+      includeMintTransactionLogs,
+      includeTokenUri,
+      includeMakerReverseProfile,
+      includeTakerReverseProfile,
     });
 
     return data.address;
