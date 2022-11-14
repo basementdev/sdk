3a6697d70caac743dc11503d372942bd738fc4b9
Anas Yousef <anas.y0807@gmail.com>
2022-11-14 16:41:33 +0000

    feat: implement transaction query

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,47 @@
 import { TokensFilter, TokensQueryVariables } from "./sdk";
 
-export type OwnerOptions = {
+// https://stackoverflow.com/questions/40510611/typescript-interface-require-one-of-two-properties-to-exist
+type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
+  }[Keys];
+
+export type IncludeFullProfileOptions = {
   /** Whether to include the ENS profile information in the response - defaults to `false` */
-  profile?: boolean;
+  profile: boolean;
   /** Whether to include the reverse resolution of the ENS profile information in the response (ENS docs: https://docs.ens.domains/contract-api-reference/reverseregistrar) - defaults to `false`  */
-  reverseProfile?: boolean;
+  reverseProfile: boolean;
 };
 
+export type IncludeOnlyReverseProfile = Pick<
+  IncludeFullProfileOptions,
+  "reverseProfile"
+>;
+
 export type SalesFilterOptions = {
-  maker?: Required<Pick<OwnerOptions, "reverseProfile">>;
-  taker?: Required<Pick<OwnerOptions, "reverseProfile">>;
+  maker: IncludeOnlyReverseProfile | boolean;
+  taker: IncludeOnlyReverseProfile | boolean;
 };
 
 export type MintFilterOptions = {
   /** Whether to include the logs that happened within the transaction - defaults to `false` */
-  transactionLogs?: boolean;
+  transactionLogs: boolean;
 };
 
 export type TokenQueryIncludeOptions = {
-  /** Whether to include owner's information in the response. Having an empty `owner` object will only return the `owner`'s address */
-  owner?: OwnerOptions;
+  /** Whether to include owner's information in the response. */
+  owner?: IncludeFullProfileOptions | boolean;
   /** Whether to include the media attached to the token, like the image, animation, etc - defaults to `false`  */
   media?: boolean;
   /** Whether to include the tokenUri. This is directly called from the contract and given as is, in JSON format - defaults to `false` */
   tokenUri?: boolean;
-  /** Whether to include sales data. This includes information like the price at which previous sales happened and on which marketplace. Having an empty `sales` object will exclude the taker and maker data  */
-  sales?: SalesFilterOptions;
-  /** Whether to include information regarding the token's mint. This includes information like the mint transaction and mint price. Having an empty `mint` object will exclude the logs that happened within the mint transaction */
-  mint?: MintFilterOptions;
+  /** Whether to include sales data. This includes information like the price at which previous sales happened and on which marketplace.  */
+  sales?: RequireAtLeastOne<SalesFilterOptions> | boolean;
+  /** Whether to include information regarding the token's mint. This includes information like the mint transaction and mint price. */
+  mint?: MintFilterOptions | boolean;
 };
 
 export type TokenFilterOptions = {
@@ -75,10 +89,10 @@ export type TokenTransfersQueryFilterOptions = TokensQueryFilterOptions;
 export type TokenTransfersQueryIncludeOptions = {
   /** Whether to include ERC721 metadata, like `tokenId`, `attributes`, `contractAddress`, etc - defaults to `false` */
   erc721Metadata?: boolean;
-  /** Whether to include from's information in the response. Having an empty `from` object will only return the `from`'s address */
-  from?: OwnerOptions & TokensIncludeOption;
-  /** Whether to include to's information in the response. Having an empty `to` object will only return the `to`'s address */
-  to?: OwnerOptions & TokensIncludeOption;
+  /** Whether to include from's information in the response. */
+  from?: IncludeFullProfileOptions & TokensIncludeOption;
+  /** Whether to include to's information in the response. */
+  to?: IncludeFullProfileOptions & TokensIncludeOption;
 };
 
 export type TokenTransfersQueryOptions = {
@@ -92,11 +106,24 @@ export type TokenTransfersQueryOptions = {
   include?: TokenTransfersQueryIncludeOptions;
 };
 
-export type AddressQueryIncludeOptions = TokensIncludeOption & OwnerOptions;
+export type AddressQueryIncludeOptions = TokensIncludeOption &
+  IncludeFullProfileOptions;
 
 export type AddressQueryOptions = {
   /** hex-address or ENS address */
   address: string;
   /** Includes more data in the response */
   include?: AddressQueryIncludeOptions;
+};
+
+export type TransactionQueryIncludeOptions = {
+  logs?: boolean;
+  sender?: IncludeOnlyReverseProfile | boolean;
+  recipient?: IncludeOnlyReverseProfile | boolean;
+};
+
+export type TransactionQueryOptions = {
+  /** Transaction hash */
+  hash: string;
+  include?: TransactionQueryIncludeOptions;
 };
