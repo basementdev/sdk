86e96c08b43b9b6098aaec00a43c0573837dc5c2
Anas Yousef <anas.y0807@gmail.com>
2022-11-24 19:51:28 +0000

    export all types from sdk

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 export * from "./basement";
 export * from "./types";
-export {
-  AddressQuery,
-  ExcludeTransferFilter,
-  TokenQuery,
-  TokensQuery,
-  Erc721TransfersQuery,
-  TransactionLogsQuery,
-  TransactionQuery,
-  TransactionsQuery,
-  NonFungibleTokenRefreshMutation,
-} from "./sdk";
+export * from "./sdk";
