868b8bd6939d45772b48ffa371ab9ff308a40bbf
Anas Yousef <anas.y0807@gmail.com>
2022-12-07 10:15:33 +0000

    merge headers

--- src/basement.ts
+++ src/basement.ts
@@ -48,8 +48,7 @@ export class BasementSDK {
     action: (requestHeaders?: Record<string, string>) => Promise<T>
   ): Promise<T> => {
     const headers: Record<string, string> = {
-      "X-Basement-SDK": "true",
-      "X-Basement-SDK-Version": process.env.npm_package_version,
+      "X-Basement-SDK": process.env.npm_package_version,
     };
     if (this.apiKey) {
       headers["x-basement-api-key"] = this.apiKey;
